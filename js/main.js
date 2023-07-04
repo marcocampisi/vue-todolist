0960a91bd8c9c526b436af609ce224c5e11e1e22
marcocampisi <marco.campisi@outlook.it>
2023-07-04 15:02:21 +0200

    Add list generation

--- js/main.js
+++ js/main.js
@@ -3,7 +3,20 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            todos : [
+                {
+                    text : 'Primo Todo',
+                    done : false
+                },
+                {
+                    text : 'Secondo Todo',
+                    done : false
+                },
+                {
+                    text : 'Terzo Todo',
+                    done : false
+                }
+            ]
         }
     }
 }).mount('#app');
